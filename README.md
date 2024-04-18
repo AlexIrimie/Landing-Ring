c0a5693863d28259bc7faa94c6abcb9b74a352c9
Alex Irimie <alex@irimie.com>
2024-04-18 15:48:56 -0400

    Added max_angle.py

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## Code for determining the change in location of center point of landing ring given angle with horizontal
 
-run delta.py and input as needed
+run delta.py to find the displacement given a set angle
+
+run max_angle.py to find max angle with horizontal possible given a max displacement allowed
---
+++ max_angle.py
@@ -0,0 +1,39 @@
+import math
+
+import math
+
+radius = input("Enter the radius of the sphere in ft: ")
+
+o_x = 0
+o_y = 0
+o_z = radius
+
+max_dis = float(input("Enter the max displacement of the center of the ring in inches: "))
+max_dis = max_dis / 12
+
+phi = math.degrees(math.asin(max_dis / float(radius)))
+
+print(f"The max angle from the horizontal you can get is {phi}")
+
+# theta = math.radians(float(input("Choose the angle on the xy plane the ring will face (degrees): ")))
+# phi = math.radians(float(input("Choose the angle from the horizontal that the ring will face: ")))
+
+# print(theta, phi)
+
+# n_x = int(radius) * math.sin(phi) * math.cos(theta)
+# n_y = int(radius) * math.sin(phi) * math.sin(theta)
+# n_z = int(radius) * math.cos(phi)
+
+# # print(n_x, n_y, n_z)
+
+# del_x = n_x
+# del_y = n_y
+# del_z = n_z - int(o_z)
+
+# del_xy = math.sqrt((del_x * del_x) + (del_y * del_y))
+
+# print("The total xy distance from the center is " + str(del_xy) + " ft or " + str(del_xy * 12) + " in")
+
+# del_xyz = math.sqrt((del_x * del_x) + (del_y * del_y) + (del_z * del_z))
+
+# print(f"The total displacement of the center of the ring is {del_xyz} ft or {12 * del_xyz} in")
